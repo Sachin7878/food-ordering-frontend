7c6c53b9704e15361c9017a29acce1d261018436
Sachin Sheelavant <sheelavantsachin@gmail.com>
2021-01-30 19:36:09 +0530

    fixed cart responsive UI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:8080',
+  baseUrl:
+    'http://foodorderingsystembackend-env-1.eba-zjzawdir.ap-south-1.elasticbeanstalk.com',
 };
